46870721b5a36a13689cd2c4dfcab8bfd57389e2
Th. F <50710133+thomasb6@users.noreply.github.com>
2025-03-05 18:37:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dash import Dash, dcc, html, Input, Output, no_update, callback
 from skimage import io
 import io as io_buffer
 from PIL import Image
-import os 
+import os
 
 # Configuration GitHub (dépôt public)
 REPO_OWNER = "thomasb6"
@@ -14,7 +14,8 @@ REPO_NAME = "alpha-detouring-BirdCHIN"
 FOLDER_PATH = "optos_jpg"
 GITHUB_API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{FOLDER_PATH}"
 
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # Récupérer le token depuis Render
+GITHUB_TOKEN = "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"  # Récupérer le token depuis Render
+
 
 def get_filenames():
     headers = {
@@ -90,4 +91,4 @@ def on_new_annotation(relayout_data):
     return no_update
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
